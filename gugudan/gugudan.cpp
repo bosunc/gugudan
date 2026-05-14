78628c9357ae3f546e6b25b025f950968d7685f5
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 14:12:50 +0900

    ===

--- gugudan/gugudan.cpp
+++ gugudan/gugudan.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    char DS_VAL; //ice
+    char DS_VAL; //ice =====
 
     cout << "input type : DESCENDING => D, ASCENDING => A" << endl;
     cin >> DS_VAL; // input 한글
