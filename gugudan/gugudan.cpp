1f4974046e37d1ef995996e7d0d1d4120c567f53
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 13:42:42 +0900

    초기 커밋.

---
+++ gugudan/gugudan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int gugu;
+	cout << "input number : " << endl;
+	cin >> gugu;
+
+	cout << "you input " << gugu << "dan" << endl;
+	cout << "--------------------------" << endl;
+	for (int i = 1; i < 10; i++) {
+		cout << gugu << " X " << i << " = " << gugu * i << endl;
+	}
+	return 0;
+
+
+
+}
