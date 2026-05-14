a0af7e2ca8d80a4fc05209e49949d253318fe590
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 14:56:28 +0900

    rand

--- gugudan/gugudan.cpp
+++ gugudan/gugudan.cpp
@@ -2,29 +2,12 @@
 using namespace std;
 
 int main() {
-    char DS_VAL; //ice ===== 3334
+    int ran_num;
+    int num1;
 
-    cout << "input type : DESCENDING => D, ASCENDING => A" << endl;
-    cin >> DS_VAL; // input 한글 +++
+    cout << "guess?" << endl;
+    ran_num = rand();
+   // cout << ran_num << endl;
 
-    if (DS_VAL == 'a') DS_VAL = 'A';
-    if (DS_VAL == 'd') DS_VAL = 'D'; // 추가주석
-
-    if (DS_VAL != 'D' && DS_VAL != 'A') {
-        cout << "Input value error" << endl;
-        return 1; // error code // sorcr  ㅇㄹ
-    }
-
-    if (DS_VAL == 'A') { // fandan
-        for (int i = 1; i < 100; i++) {
-            cout << i << endl;
-        } // 주석추가 123
-    }
-    else if (DS_VAL == 'D') {
-        for (int i = 99; i > 0; i--) {
-            cout << i << endl; // end ;;
-        }
-    }
-    //dificult level 12
     return 0;
 }
