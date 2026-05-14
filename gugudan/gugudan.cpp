aeeaedb052b374a1ea9a69bc431a2b94f0f3fdff
SKS_hanryang <bosun1985@gmail.com>
2026-05-14 15:36:30 +0900

    변경

--- gugudan/gugudan.cpp
+++ gugudan/gugudan.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 int main() {
     int ran_num;
     int num1;
+
+    srand(time(0));
+
+    ran_num = rand() % 100 + 1;
 
     cout << "guess?" << endl;
     ran_num = rand();
@@ -11,10 +18,10 @@ int main() {
   
     while (num1 != ran_num) {
         if (num1 > ran_num) {
-            cout << "bigger than number" <<  endl;
+            cout << "down" <<  endl;
         }
         else {
-            cout << "smaller than number" <<  endl;
+            cout << "up" <<  endl;
 
         }
         
